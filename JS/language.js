04cbf75ae80f4cc0c30fef8a78399801004400cb
Airdropcel <muhamadrivaioke@gmail.com>
2026-08-08 22:19:50 +0700

    Create language.js

---
+++ JS/language.js
@@ -0,0 +1,23 @@
+const lang = {
+
+id:{
+    heroTitle:"TAHU GEJROT PAKDE BURUNG"
+},
+
+en:{
+    heroTitle:"PAKDE BURUNG TOFU GEJROT"
+},
+
+zh:{
+    heroTitle:"Pakde Burung 印尼特色豆腐"
+},
+
+ja:{
+    heroTitle:"パクデ・ブルン 豆腐ゲジョロット"
+},
+
+ko:{
+    heroTitle:"박데 부룽 타후 게조롯"
+}
+
+};
