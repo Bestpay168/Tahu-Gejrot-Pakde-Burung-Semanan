48c0a13303fa29980f4409da0b881b46f35221f8
Airdropcel <muhamadrivaioke@gmail.com>
2026-08-13 16:49:07 +0700

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -901,3 +901,26 @@ copyright.innerHTML=
 `© ${year} TAHU GEJROT PAKDE BURUNG`;
 
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const modal = document.getElementById("reservationModal");
+    const open = document.getElementById("openReservation");
+    const close = document.getElementById("closeReservation");
+
+    open.addEventListener("click", function(e){
+        e.preventDefault();
+        modal.classList.add("show");
+    });
+
+    close.addEventListener("click", function(){
+        modal.classList.remove("show");
+    });
+
+    modal.addEventListener("click", function(e){
+        if(e.target === modal){
+            modal.classList.remove("show");
+        }
+    });
+
+});
